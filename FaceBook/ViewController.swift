ea8bb2e09a84a923004b8962d25503a4089693a4
sh <shhang.2021@smu.edu.sg>
2025-10-16 14:29:19 +0800

    Initial Commit

---
+++ FaceBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceBook
+//
+//  Created by Sing Hui Hang on 16/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
